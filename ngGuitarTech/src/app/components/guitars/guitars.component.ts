e457a1f75e466b2dae4ec548858a88091713e910
Justin Martz <justinmartz@protonmail.com>
2023-10-10 16:01:43 -0600

    CSS updates to landing page. Added custom bullet icons

--- ngGuitarTech/src/app/components/guitars/guitars.component.ts
+++ ngGuitarTech/src/app/components/guitars/guitars.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-guitars',
   templateUrl: './guitars.component.html',
   styleUrls: ['./guitars.component.css']
 })
-export class GuitarsComponent {
+export class GuitarsComponent implements OnInit {
 
+  guitarsSelected: boolean = false;
+  @Output() emitter: EventEmitter<string> = new EventEmitter<string>();
+
+  ngOnInit(): void {
+    this.guitarsSelected = true;
+  }
 }
