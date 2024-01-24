f9ad72a4cc8174a0f9dda77220dc3a4f0d9c0b50
Justin Martz <justinmartz@protonmail.com>
2024-01-24 15:22:33 -0700

    Registering a new user implemented: last_login and visibility not yet modified on creation

--- ngGuitarTech/src/app/components/login-modal-link/login-modal-link.component.ts
+++ ngGuitarTech/src/app/components/login-modal-link/login-modal-link.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef, ViewChild } from '@angular/core';
 import { User } from '../../models/user';
 import { Guitar } from '../../models/guitar';
 import { GuitarPicture } from '../../models/guitar-picture';
@@ -12,29 +12,30 @@ import { ToastService } from '../../services/toast.service';
 @Component({
   selector: 'app-login-modal-link',
   templateUrl: './login-modal-link.component.html',
-  styleUrls: ['./login-modal-link.component.css']
+  styleUrls: ['./login-modal-link.component.css'],
 })
 export class LoginModalLinkComponent {
   closeResult = '';
   loginOpen: boolean = true;
   modalOpen: boolean = false;
-  newUser: User = new User();
 
   loginForm = new FormGroup({
     loginUsername: new FormControl('', Validators.required),
-    loginPassword: new FormControl('', Validators.required)
+    loginPassword: new FormControl('', Validators.required),
   });
 
   registrationForm = new FormGroup({
     registrationUsername: new FormControl('', Validators.required),
-    registrationPassword: new FormControl('', Validators.required)
+    registrationPassword: new FormControl('', Validators.required),
   });
 
-	constructor(private modalService: NgbModal,
+  constructor(
+    private modalService: NgbModal,
     private authService: AuthService,
     private router: Router,
     private appServ: AppService,
-    private toastServ: ToastService) {}
+    private toastServ: ToastService
+  ) {}
 
   ngOnInit(): void {
     this.loginOpen = true;
@@ -42,24 +43,21 @@ export class LoginModalLinkComponent {
     this.registrationForm.reset();
   }
 
-	open(content: any) {
+  open(content: any) {
     this.modalOpen = true;
-		this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then(
-			(result) => {
-				this.closeResult = `Closed with: ${result}`;
-        this.modalOpen = false;
-        if (this.loginOpen) {
-
-        } else {
-          // register
+    this.modalService
+      .open(content, { ariaLabelledBy: 'modal-basic-title' })
+      .result.then(
+        (result) => {
+          this.closeResult = `Closed with: ${result}`;
+          this.modalOpen = false;
+        },
+        (reason) => {
+          this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+          this.modalOpen = false;
         }
-			},
-			(reason) => {
-				this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-        this.modalOpen = false;
-			},
-		);
-	}
+      );
+  }
 
   onLoginClick(content: any) {
     this.loginOpen = true;
@@ -75,9 +73,11 @@ export class LoginModalLinkComponent {
 
   onSubmitClick() {
     if (this.loginOpen) {
+      console.log('logging in existing user');
       // construct new user object from form values
       let tmpUser = this.loginForm.value.loginUsername;
       let tmpPass = this.loginForm.value.loginPassword;
+
       if (tmpUser != null) {
         this.newLoginUser.username = tmpUser;
       }
@@ -88,19 +88,33 @@ export class LoginModalLinkComponent {
 
       this.login(this.newLoginUser);
       this.modalService.dismissAll();
-    }
+    } else {
+      console.log('register modal open and submit clicked');
+      // construct new user object from form values
+      let tmpUser = this.registrationForm.value.registrationUsername;
+      let tmpPass = this.registrationForm.value.registrationPassword;
+      if (tmpUser != null) {
+        this.newRegistrationUser.username = tmpUser;
+      }
 
+      if (tmpPass != null) {
+        this.newRegistrationUser.password = tmpPass;
+      }
+
+      this.register(this.newRegistrationUser);
+      this.modalService.dismissAll();
+    }
   }
 
-	private getDismissReason(reason: any): string {
-		if (reason === ModalDismissReasons.ESC) {
-			return 'by pressing ESC';
-		} else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-			return 'by clicking on a backdrop';
-		} else {
-			return `with: ${reason}`;
-		}
-	}
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
 
   register(user: User): void {
     this.authService.register(user).subscribe({
@@ -114,15 +128,19 @@ export class LoginModalLinkComponent {
             }
           },
           error: (problem) => {
-            console.error('LoginRegisterModalComponent.register(): Error logging in user:');
+            console.error(
+              'LoginModalLinkComponent.register(): Error logging in user:'
+            );
             console.error(problem);
-          }
+          },
         });
       },
       error: (fail) => {
-        console.error('LoginRegisterModalComponent.register(): Error registering account');
+        console.error(
+          'LoginModalLinkComponent.register(): Error registering account'
+        );
         console.error(fail);
-      }
+      },
     });
   }
 
@@ -136,10 +154,15 @@ export class LoginModalLinkComponent {
         }
       },
       error: (problem) => {
-        console.error('LoginRegisterModalComponent.login(): Error logging in user:');
+        console.error(
+          'LoginModalLinkComponent.login(): Error logging in user:'
+        );
         console.error(problem);
-        this.toastServ.show('Error logging in.', { classname: 'bg-danger text-light', delay: 3000 });
-      }
+        this.toastServ.show('Error logging in.', {
+          classname: 'bg-danger text-light',
+          delay: 3000,
+        });
+      },
     });
   }
 
@@ -151,21 +174,45 @@ export class LoginModalLinkComponent {
     }
   }
 
-  autoFill() {
-    this.loginForm.setValue({loginUsername: 'sally', loginPassword: '1234'});
+  openLoginForm(content: any) {
+    this.loginOpen = true;
+    this.open(content);
   }
 
-  get loginUsername() { return this.loginForm.get('loginUsername'); }
+  openRegisterForm(content: any) {
+    this.loginOpen = false;
+    this.open(content);
+  }
 
-  get loginPassword() { return this.loginForm.get('loginPassword'); }
+  autoFill() {
+    this.loginForm.setValue({ loginUsername: 'sally', loginPassword: '1234' });
+  }
 
-  get registrationUsername() { return this.registrationForm.get('registrationUsername'); }
+  get loginUsername() {
+    return this.loginForm.get('loginUsername');
+  }
 
-  get registrationPassword() { return this.registrationForm.get('registrationPassword'); }
+  get loginPassword() {
+    return this.loginForm.get('loginPassword');
+  }
+
+  get registrationUsername() {
+    return this.registrationForm.get('registrationUsername');
+  }
+
+  get registrationPassword() {
+    return this.registrationForm.get('registrationPassword');
+  }
 
   get newLoginUser() {
     const o: any = {};
     Object.assign(o, this.loginForm.value);
     return new User(0, o.loginUsername, o.loginPassword, 'player');
+  }
+
+  get newRegistrationUser() {
+    const o: any = {};
+    Object.assign(o, this.registrationForm.value);
+    return new User(0, o.registrationUsername, o.registrationPassword, 'player');
   }
 }
--- ngGuitarTech/src/app/components/login-register-modal/login-register-modal.component.ts
+++ ngGuitarTech/src/app/components/login-register-modal/login-register-modal.component.ts
@@ -19,23 +19,24 @@ export class LoginRegisterModalComponent implements OnInit {
   closeResult = '';
   loginOpen: boolean = true;
   modalOpen: boolean = false;
-  newUser: User = new User();
 
   loginForm = new FormGroup({
     loginUsername: new FormControl('', Validators.required),
-    loginPassword: new FormControl('', Validators.required)
+    loginPassword: new FormControl('', Validators.required),
   });
 
   registrationForm = new FormGroup({
     registrationUsername: new FormControl('', Validators.required),
-    registrationPassword: new FormControl('', Validators.required)
+    registrationPassword: new FormControl('', Validators.required),
   });
 
-	constructor(private modalService: NgbModal,
+  constructor(
+    private modalService: NgbModal,
     private authService: AuthService,
     private router: Router,
     private appServ: AppService,
-    private toastServ: ToastService) {}
+    private toastServ: ToastService
+  ) {}
 
   ngOnInit(): void {
     this.loginOpen = true;
@@ -43,24 +44,21 @@ export class LoginRegisterModalComponent implements OnInit {
     this.registrationForm.reset();
   }
 
-	open(content: any) {
+  open(content: any) {
     this.modalOpen = true;
-		this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then(
-			(result) => {
-				this.closeResult = `Closed with: ${result}`;
-        this.modalOpen = false;
-        if (this.loginOpen) {
-
-        } else {
-          // register
+    this.modalService
+      .open(content, { ariaLabelledBy: 'modal-basic-title' })
+      .result.then(
+        (result) => {
+          this.closeResult = `Closed with: ${result}`;
+          this.modalOpen = false;
+        },
+        (reason) => {
+          this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+          this.modalOpen = false;
         }
-			},
-			(reason) => {
-				this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-        this.modalOpen = false;
-			},
-		);
-	}
+      );
+  }
 
   onLoginClick(content: any) {
     this.loginOpen = true;
@@ -76,9 +74,11 @@ export class LoginRegisterModalComponent implements OnInit {
 
   onSubmitClick() {
     if (this.loginOpen) {
+      console.log('logging in existing user');
       // construct new user object from form values
       let tmpUser = this.loginForm.value.loginUsername;
       let tmpPass = this.loginForm.value.loginPassword;
+
       if (tmpUser != null) {
         this.newLoginUser.username = tmpUser;
       }
@@ -89,50 +89,81 @@ export class LoginRegisterModalComponent implements OnInit {
 
       this.login(this.newLoginUser);
       this.modalService.dismissAll();
-    }
+    } else {
+      console.log('register modal open and submit clicked');
+      // construct new user object from form values
+      let tmpUser = this.registrationForm.value.registrationUsername;
+      let tmpPass = this.registrationForm.value.registrationPassword;
+      if (tmpUser != null) {
+        this.newRegistrationUser.username = tmpUser;
+      }
 
+      if (tmpPass != null) {
+        this.newRegistrationUser.password = tmpPass;
+      }
+
+      this.register(this.newRegistrationUser);
+      this.modalService.dismissAll();
+    }
   }
 
-	private getDismissReason(reason: any): string {
-		if (reason === ModalDismissReasons.ESC) {
-			return 'by pressing ESC';
-		} else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-			return 'by clicking on a backdrop';
-		} else {
-			return `with: ${reason}`;
-		}
-	}
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
 
   register(user: User): void {
     this.authService.register(user).subscribe({
       next: (registeredUser) => {
         this.authService.login(user.username, user.password).subscribe({
           next: (loggedInUser) => {
-            this.router.navigateByUrl('/guitars');
+            if (window.innerWidth >= 1280) {
+              this.router.navigateByUrl('/desktop');
+            } else {
+              this.router.navigateByUrl('/guitars');
+            }
           },
           error: (problem) => {
-            console.error('LoginRegisterModalComponent.register(): Error logging in user:');
+            console.error(
+              'LoginModalLinkComponent.register(): Error logging in user:'
+            );
             console.error(problem);
-          }
+          },
         });
       },
       error: (fail) => {
-        console.error('LoginRegisterModalComponent.register(): Error registering account');
+        console.error(
+          'LoginModalLinkComponent.register(): Error registering account'
+        );
         console.error(fail);
-      }
+      },
     });
   }
 
   login(user: User) {
     this.authService.login(user.username, user.password).subscribe({
       next: (loggedInUser) => {
-        this.router.navigateByUrl('/guitars');
+        if (window.screen.width >= 1280) {
+          this.router.navigateByUrl('/desktop');
+        } else {
+          this.router.navigateByUrl('/guitars');
+        }
       },
       error: (problem) => {
-        console.error('LoginRegisterModalComponent.login(): Error logging in user:');
+        console.error(
+          'LoginModalLinkComponent.login(): Error logging in user:'
+        );
         console.error(problem);
-        this.toastServ.show('Error logging in.', { classname: 'bg-danger text-light', delay: 3000 });
-      }
+        this.toastServ.show('Error logging in.', {
+          classname: 'bg-danger text-light',
+          delay: 3000,
+        });
+      },
     });
   }
 
@@ -144,21 +175,45 @@ export class LoginRegisterModalComponent implements OnInit {
     }
   }
 
-  autoFill() {
-    this.loginForm.setValue({loginUsername: 'sally', loginPassword: '1234'});
+  openLoginForm(content: any) {
+    this.loginOpen = true;
+    this.open(content);
   }
 
-  get loginUsername() { return this.loginForm.get('loginUsername'); }
+  openRegisterForm(content: any) {
+    this.loginOpen = false;
+    this.open(content);
+  }
 
-  get loginPassword() { return this.loginForm.get('loginPassword'); }
+  autoFill() {
+    this.loginForm.setValue({ loginUsername: 'sally', loginPassword: '1234' });
+  }
 
-  get registrationUsername() { return this.registrationForm.get('registrationUsername'); }
+  get loginUsername() {
+    return this.loginForm.get('loginUsername');
+  }
 
-  get registrationPassword() { return this.registrationForm.get('registrationPassword'); }
+  get loginPassword() {
+    return this.loginForm.get('loginPassword');
+  }
+
+  get registrationUsername() {
+    return this.registrationForm.get('registrationUsername');
+  }
+
+  get registrationPassword() {
+    return this.registrationForm.get('registrationPassword');
+  }
 
   get newLoginUser() {
     const o: any = {};
     Object.assign(o, this.loginForm.value);
     return new User(0, o.loginUsername, o.loginPassword, 'player');
+  }
+
+  get newRegistrationUser() {
+    const o: any = {};
+    Object.assign(o, this.registrationForm.value);
+    return new User(0, o.registrationUsername, o.registrationPassword, 'player');
   }
 }
